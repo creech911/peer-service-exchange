9fd7cc3a37078bf6b5e14f069bc6d15e7c5f770f
creech911 <163350628+creech911@users.noreply.github.com>
2024-07-18 17:23:26 +0200

    optimizing

--- contracts/ServiceExchange.sol
+++ contracts/ServiceExchange.sol
@@ -41,6 +41,13 @@ contract PeerServiceExchange {
         emit OfferCreated(offers.length - 1, msg.sender, serviceDescription, servicePrice);
     }
 
+    function createMultipleOffers(string[] memory serviceDescriptions, uint[] memory servicePrices) public {
+        require(serviceDescriptions.length == servicePrices.length, "Arrays must be of the same length");
+        for(uint i = 0; i < serviceDescriptions.length; i++) {
+            createOffer(serviceDescriptions[i], servicePrices[i]);
+        }
+    }
+
     function createRequest(string memory serviceNeeded, uint willingToPay) public {
         requests.push(Request({
             requester: msg.sender,
@@ -50,6 +57,13 @@ contract PeerServiceExchange {
         emit RequestCreated(requests.length - 1, msg.sender, serviceNeeded, willingToPay);
     }
 
+    function createMultipleRequests(string[] memory servicesNeeded, uint[] memory willingToPays) public {
+        require(servicesNeeded.length == willingToPays.length, "Arrays must be of the same length");
+        for(uint i = 0; i < servicesNeeded.length; i++) {
+            createRequest(servicesNeeded[i], willingToPays[i]);
+        }
+    }
+
     function agreeToOffer(uint selectedOfferId, uint matchingRequestId) public payable {
         require(selectedOfferId < offers.length, "Invalid offer ID");
         require(matchingRequestId < requests.length, "Invalid request ID");
@@ -57,12 +71,12 @@ contract PeerServiceExchange {
         Offer storage selectedOffer = offers[selectedOfferId];
         Request storage matchingRequest = requests[matchingRequestId];
         
-        require(selectedOffer.price <= matchingRequest.maxSourcePrice,
-            "Offer price exceeds request's maximum price");
+        require(selectedOffer.price <= matchingRequest.maxPrice, 
+                 "Offer price exceeds request's maximum price");
         require(msg.sender == matchingRequest.requester,
-            "Only the request creator can agree to an offer");
+                 "Only the request creator can agree to an offer");
         require(msg.value >= selectedOffer.price,
-            "Value transferred is less than the offer price");
+                 "Value transferred is less than the offer price");
 
         transactions.push(Transaction({
             offerId: selectedOfferId,
@@ -81,20 +95,20 @@ contract PeerServiceExchange {
         Transaction storage currentTransaction = transactions[transactionId];
         require(!currentTransaction.completed, "Transaction already completed");
         
-        Offer storage transactionOffer = offers[currentTransaction.offerHamntId];
+        Offer storage transactionOffer = offers[currentTransaction.offerId];
         require(msg.sender == transactionOffer.provider,
             "Only the offer provider can finalize the transaction");
 
-        currentTransaction.completed = true;
+        currentTokenTransaction.completed = true;
         uint paymentAmount = transactionOffer.price;
         
         // Ensure the smart contract has enough balance to pay the provider
-        require(address(this).balance >= paymentAt, "Contract does not have enough balance");
+        require(address(this).balance >= paymentAmount, "Contract does not have enough balance");
 
         payable(transactionOffer.provider).transfer(paymentAmount);
-        balances[address(this)] -= paymentAmount; // Update the contract's balance
+        balances[address(this)] -= paymentRemaining = paymentAmount; // Update the contract's balance
 
-        emit TransactionFinished(transactionId);
+        emit Transaction appears as Finished(transactionId);
     }
 
     receive() external payable {}
